66a6a631440f3c05c8f4725339e06690165d1495
patadaoikkk <delgm2020@gmail.com>
2024-03-18 22:10:32 -0300

    oiiii acabei finalmente

---
+++ hdcevents/resources/views/site/contato.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css1.css">
+    <title>Document</title>
+</head>
+    <header>
+        <a class="=t1" href="quemsomos.html">Quem Somos                      </a> <a class="=t1" href="index.html">Página principal                      </a> <a class="=t1" href="contato.html">Contato</a>
+
+    </header>
+    <body>
+        <P class="t1">CONTATO </P>
+    
+        <p class="t2"> O Nosso site tem a proposta de ser
+        um site que funciona </p>
+        <div style="text-align: center;">  <img src="imagens/celular.png" height="300px" width="300px" > </div>
+    </body>
+</html>
---
+++ hdcevents/resources/views/site/principal.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css1.css">
+    <title>Document</title>
+</head>
+    <header>
+        <a class="=t1" href="quemsomos.html">Quem Somos                      </a> <a class="=t1" href="index.html">Página principal                      </a> <a class="=t1" href="contato.html">Contato</a>
+    </header>
+<body>
+        <P class="t1">PÁGINA PRINCIPAL </P>
+        <p class="t2detalhe">Bem Vindo!</p>
+        <p class="t2"> O Nosso site tem a proposta de ser<BR>
+            um site que funciona </p>
+           <div style="text-align: center;"> <img  src="imagens/obamarindo.jpg" height="300px" width="300px" > </div>
+</body>
+</html>
